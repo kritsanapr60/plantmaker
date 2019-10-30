b73c85bc84b617fbd8f055546add4bb60766b645
Ahom <33215457+luangjohn@users.noreply.github.com>
2019-10-30 16:00:15 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/mapmaker', 'AbtController@home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
