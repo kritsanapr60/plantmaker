2600c3a53c4bfc48b9d89236ec0efa640109c40a
Ahom <33215457+luangjohn@users.noreply.github.com>
2019-10-30 16:03:18 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/map', function () {
+    return view('welcome');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
