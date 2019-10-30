25dcac5b568763fe6bf7ab48a31301023a654903
Ahom <33215457+luangjohn@users.noreply.github.com>
2019-10-30 16:06:48 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/map', function () {
     return view('index');
 });
 
-Route::get('/map', function () {
+Route::get('/', function () {
     return view('welcome');
 });
 
