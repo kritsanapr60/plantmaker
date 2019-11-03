36e265f85554e76bf50a67aab1e1e9f6796a2d35
Ahom <33215457+luangjohn@users.noreply.github.com>
2019-11-03 20:41:52 +0700

    Update apikey.js

--- public/js/apikey.js
+++ public/js/apikey.js
@@ -1,3 +1,3 @@
 var config = {
-  apiKey : 'AIzaSyByxAxM_f64MaGCNie0y3joCJmUUA_TRZE'
+  apiKey : "https://maps.googleapis.com/maps/api/js?key=AIzaSyByxAxM_f64MaGCNie0y3joCJmUUA_TRZE&callback=initMap"
 }
