4168c6566792cced7c5f2f69beed6b162ed145ab
Ahom <33215457+luangjohn@users.noreply.github.com>
2019-11-03 19:45:24 +0700

    Update sidebar.blade.php

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -7,30 +7,10 @@
         <div class="card-body">
             <ul class="nav flex-column" role="tablist">
                 <li class="nav-item" role="presentation">
-                    <a href="{{ url('/admin') }}">
-                        Dashboard
-                    </a>
-                </li>
-
-                <li class="nav-item" role="presentation">
                     <a href="{{ url('/admin/user') }}">
                         User
                     </a>
                 </li>
-
-
-               <li class="nav-item" role="presentation">
-                    <a href="{{ url('/admin/role') }}">
-                        Role
-                    </a>
-                </li>
- <!-- 
-
-                <li class="nav-item" role="presentation">
-                    <a href="{{ url('/admin/permission') }}">
-                        Permission
-                    </a>
-                </li> -->
 
             </ul>
         </div>
