77bc5a1f0c3a79c296b23949b780ba2955cec22e
Ahom <33215457+luangjohn@users.noreply.github.com>
2019-11-03 19:27:11 +0700

    Update AbtController.php

--- app/Http/Controllers/AbtController.php
+++ app/Http/Controllers/AbtController.php
@@ -36,7 +36,7 @@ class AbtController extends Controller
                 'ระดับ [status] </b></p>'.
             '</div>'.
             '<div id="bodyContent" class="col-md-4">'.
-                '<img src="/~plant/plantmaker/public/image_school/[nimg] " alt="Smiley face" height="480" width="720">'.
+                '<img src="/~plant/plantmaker/public/image_school/[nimg] " alt="Smiley face" height="230" width="300">'.
             '</div>'.
         '</div>';
         
