7cf3f0178011f2666de95040d9ce7c0bedbd98fa
kritsanapr60 <33215464+kritsanapr60@users.noreply.github.com>
2019-11-03 16:10:09 +0700

    แก้ไข Icon ไม่ขึ้น

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login - Pag</title>
-
+    
+    <link rel="shortcut icon" href="{{ asset('img/logo.gif') }}" />
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
