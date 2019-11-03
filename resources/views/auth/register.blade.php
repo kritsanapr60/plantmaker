4aa7dd2f95b122bdc1d177eaf80c89126b1d0859
Ahom <33215457+luangjohn@users.noreply.github.com>
2019-11-03 19:54:20 +0700

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -43,53 +43,6 @@
     </style>
 </head>
 <body style="background-color: #a400d6;">     
-        <br><br>
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-12">
-                <a href="{{ route('login') }}">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-reply-all fa-2x" aria-hidden="true" ></i></a>
-            </div>
-        </div>
-    </div>
-    <!-- <div class="container"><a type="button" href="index.html" class="btn btn-primary fadeInDown">Primary</a></div> -->
-    <div class="wrapper fadeInDown">
-        <div id="formContent">
-            <!-- Tabs Titles -->
-            
-            <!-- Icon -->
-            <div class="fadeIn first">
-                <img src="img/logo.gif" id="icon" alt="User Icon" />
-            </div>
-            <br>
-            <!-- Login Form -->
-            <form method="POST" action="{{ route('register') }}">
-            @csrf
-                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror fadeIn second" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus placeholder="Name">
-                    @error('name')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                <input id="username" type="text" class="form-control @error('username') is-invalid @enderror fadeIn second" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus placeholder="User Name">
-                    @error('username')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror " name="password" required autocomplete="new-password" placeholder="password">
-                        @error('password')
-                            <span class="invalid-feedback" role="alert">
-                                <strong>{{ $message }}</strong>
-                            </span>
-                        @enderror
-                        <input id="password-confirm" type="password" class="form-control fadeIn third" name="password_confirmation" required autocomplete="new-password" placeholder="confirm password">
-                <br>
-                <hr width="200px">
-                <button type="submit" class="btn navbar-custom text-white">{{ __('Register') }}</button>
-            </form>
-<br>
-        </div>
-    </div>
 </body>
 
 </html>
