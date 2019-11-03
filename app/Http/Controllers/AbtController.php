9b87bf65f28381d2feb0614b3004192ef4f05fb8
Ahom <33215457+luangjohn@users.noreply.github.com>
2019-11-03 18:17:29 +0700

    Update AbtController.php

--- app/Http/Controllers/AbtController.php
+++ app/Http/Controllers/AbtController.php
@@ -28,11 +28,13 @@ class AbtController extends Controller
         $zoom = 9;
 
         $abts = Abt::all();
-        $contentString = '<div id="content" class="form-group col-md-12">'.
+        $contentString = '<div id="content" class="form-group col-md-7">'.
         '<a href="/~plant/plantmaker/public/detail/[place_name]"><h1 id="firstHeading" class="firstHeading">[place_name]</h1></a>'.
         '<div id="bodyContent" class="col">'.
         '<p><b>[place_name]</b><br> ที่อยู่ [address] จังหวัด [province]<br>'.
         'ระดับ [status] </b></p>'.
+        '</div>'.
+        '<div col-md-5>'.'<img src="https://upload.wikimedia.org/wikipedia/en/thumb/6/63/IMG_%28business%29.svg/1200px-IMG_%28business%29.svg.png" alt="Smiley face" width="42" height="42">'.
         '</div>'.
         '</div>';
 
