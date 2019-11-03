a591bc78178892dee661c7b6e32818cc6970a41c
Ahom <33215457+luangjohn@users.noreply.github.com>
2019-11-03 17:09:25 +0700

    Update detail.blade.php

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -167,7 +167,7 @@
                         </ol>
                         <div class="carousel-inner">
                           <div class="carousel-item active">
-                            <img class="d-block w-100" src="{{ asset('img/la.jpg') }}" alt="First slide">
+                            <img class="d-block w-100" src="{{ asset('img_'.$data->Name.'_1.jpg') }}" alt="First slide">
                           </div>
                           <div class="carousel-item">
                             <img class="d-block w-100" src="{{ asset('img/chicago.jpg') }}" alt="Second slide">
