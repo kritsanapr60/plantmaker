76ab1aba3ce25275906941b04b1ce4f3683d3000
Ahom <33215457+luangjohn@users.noreply.github.com>
2019-11-03 19:40:47 +0700

    Update LoginController.php

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -18,7 +19,9 @@ class LoginController extends Controller
     |
     */
 
-    use AuthenticatesUsers;
+    use AuthenticatesUsers {
+        logout as performLogout;
+    }
 
     /**
      * Where to redirect users after login.
@@ -57,4 +60,9 @@ class LoginController extends Controller
     {
         return 'username';
     }
+    public function logout(Request $request)
+{
+    $this->performLogout($request);
+    return redirect('/map');
+}
 }
