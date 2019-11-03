88fc2a7683b237f6e7d01ecbaa9a5ae391d9eb37
Ahom <33215457+luangjohn@users.noreply.github.com>
2019-11-03 20:31:46 +0700

    Update googlemap.js

--- public/js/googlemap.js
+++ public/js/googlemap.js
@@ -1,1 +1,3 @@
-async="" defer="" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyByxAxM_f64MaGCNie0y3joCJmUUA_TRZE&amp;callback=initMap">
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyByxAxM_f64MaGCNie0y3joCJmUUA_TRZE&callback=initMap">
+    </script>
