71cd5a2a0678c09fe175e9006d01dcfde5653aee
Ahom <33215457+luangjohn@users.noreply.github.com>
2019-11-03 18:19:09 +0700

    Update AbtController.php

--- app/Http/Controllers/AbtController.php
+++ app/Http/Controllers/AbtController.php
@@ -34,7 +34,7 @@ class AbtController extends Controller
         '<p><b>[place_name]</b><br> ที่อยู่ [address] จังหวัด [province]<br>'.
         'ระดับ [status] </b></p>'.
         '</div>'.
-        '<div col-md-5>'.'<img src="https://upload.wikimedia.org/wikipedia/en/thumb/6/63/IMG_%28business%29.svg/1200px-IMG_%28business%29.svg.png" alt="Smiley face" width="42" height="42">'.
+        '<div class="col-md-5">'.'<img src="https://upload.wikimedia.org/wikipedia/en/thumb/6/63/IMG_%28business%29.svg/1200px-IMG_%28business%29.svg.png" alt="Smiley face" width="42" height="42">'.
         '</div>'.
         '</div>';
 
