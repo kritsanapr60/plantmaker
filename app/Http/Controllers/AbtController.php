1a772ca5fa3709e16a07c355a4357e6c06137755
Ahom <33215457+luangjohn@users.noreply.github.com>
2019-11-03 18:50:09 +0700

    Update AbtController.php

--- app/Http/Controllers/AbtController.php
+++ app/Http/Controllers/AbtController.php
@@ -36,7 +36,7 @@ class AbtController extends Controller
                 'ระดับ [status] </b></p>'.
             '</div>'.
             '<div id="bodyContent" class="col-md-6">'.
-                '<img src="http://qnimate.com/wp-content/uploads/2014/03/images2.jpg" alt="Smiley face" height="100" width="100">'.
+                '<img src="http://qnimate.com/wp-content/uploads/2014/03/images2.jpg" alt="Smiley face" height="50%" width="50%">'.
             '</div>'.
         '</div>';
 
