e183484edfd621324c33c9db508dab6893d754d7
Ahom <33215457+luangjohn@users.noreply.github.com>
2019-11-03 19:34:28 +0700

    Update detail.blade.php

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -91,7 +91,7 @@
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="{{ url('/home') }}">
-                                         Profile
+                                         แก้ไขข้อมูล
                                      </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
