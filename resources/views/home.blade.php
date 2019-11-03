85c30ec77e72e42c0db38a19c01e054d4ede2968
Ahom <33215457+luangjohn@users.noreply.github.com>
2019-11-03 16:36:21 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,16 +22,13 @@
 
                 <div class="form-group">
                     <label for="exampleFormControlSelect1">จังหวัด</label>
-                    <input class="form-control" type="text" value="{{ $data->Province }}" readonly>
-                    {{-- <select class="form-control" name="province" id="exampleFormControlSelect1">
-                    </select> --}}
+                    <input class="form-control" type="text" value="{{ $data->Province }}">
 
                 </div>
                 <div class="form-group">
                     <label for="exampleFormControlSelect1">ระดับ</label>
-                    <input class="form-control" type="text" value="{{ $data->Status }}" readonly>
-                    {{--<select class="form-control" name="status" value="{{ $data->Status }} id="exampleFormControlSelect1">
-                    </select>--}}
+                    <input class="form-control" type="text" value="{{ $data->Status }}">
+
                 </div>
                 <div class="form-group">
                     <label for="exampleFormControlInput1">วันที่เข้าร่วมโครงการ</label>
