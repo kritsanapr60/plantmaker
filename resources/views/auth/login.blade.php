0682d00e2f76c17626b62c44a5549c92c6335dba
Ahom <33215457+luangjohn@users.noreply.github.com>
2019-11-04 14:19:37 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login - Pag</title>
+    <title>UBU API : Login</title>
     
     <link rel="shortcut icon" href="{{ asset('img/logo.gif') }}" />
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
