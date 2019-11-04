a23b7728448ed97c4a8c85d6a9051b491974d658
Ahom <33215457+luangjohn@users.noreply.github.com>
2019-11-04 14:42:04 +0700

    Update detail.blade.php

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -90,9 +90,16 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ url('/home') }}">
-                                         แก้ไขข้อมู
-                                     </a>
+                                @if (Auth::user()->hasRole('super-admin')) 
+                             <a class="dropdown-item" href="{{ asset('admin/user') }}">
+                                    <i class="fas fa-user-edit"></i> เเก้ไขข้อมูล
+                                 </a>
+
+                            @else 
+                                <a class="dropdown-item" href="{{ asset('/home') }}">
+                                    <i class="fas fa-user-edit"></i> เเก้ไขข้อมูล
+                                 </a>
+                                 @endif
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
